a3342aedb1001b8cc616f19a2ec963cd675a752c
Raffay Sajjad <raffaysajjad.apple@gmail.com>
2020-10-23 01:06:40 +0500

    Added MVC support

---
+++ Controllers/HomeController.cs
@@ -0,0 +1,15 @@
+﻿using System;
+using Microsoft.AspNetCore.Mvc;
+namespace Book_Store.Controllers
+{
+    public class HomeController : Controller
+    {
+        public HomeController()
+        {
+        }
+        public string Index()
+        {
+            return "Book Store";
+        }
+    }
+}
